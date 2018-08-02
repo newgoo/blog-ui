da3451959f889862e809ea10be77a8ee3e512082
lives <happs.lives@6619.com>
2018-08-02 10:37:14 +0800

    update index

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -1,24 +1,44 @@
 <template>
   <div>
-    <card>
-      <p slot="title">
-        热门文章
-      </p>
-      <ul>
-        <li v-for="blog in HotList" :key="blog.id">
-          <router-link :to="{name:'detail',params:{id: blog.id}}">
-            {{blog.title}}
-          </router-link>
-          <i class="right"></i>
-        </li>
-      </ul>
-      <!-- <div> -->
-      <!-- <router-link :to="{name:'detail',params:{id: blog.id}}">
-        {{blog.title}}
-      </router-link> -->
-      <!-- <span class="right">fdas</span> -->
-      <!-- </div> -->
-    </card>
+    <!--<card>-->
+    <!--<p slot="title">-->
+    <!--热门文章-->
+    <!--</p>-->
+    <!--<ul>-->
+    <!--<li v-for="blog in HotList" :key="blog.id">-->
+    <!--<router-link :to="{name:'detail',params:{id: blog.id}}">-->
+    <!--{{blog.title}}-->
+    <!--</router-link>-->
+    <!--<i class="right"></i>-->
+    <!--</li>-->
+    <!--</ul>-->
+    <!--</card>-->
+
+    <div class="container_16">
+      <aside class="prefix_1 grid_5">
+        <h4>Categories</h4>
+        <ul class="categories">
+          <li><a href="#">design</a></li>
+          <li><a href="#">inspiration</a></li>
+          <li><a href="#">tutorials</a></li>
+          <li><a href="#">uncategorized</a></li>
+          <li><a href="#">updates</a></li>
+          <li><a href="#">web</a></li>
+        </ul>
+        <h4>Blogroll</h4>
+        <ul class="blogroll">
+          <li><a href="#">Documentation</a></li>
+          <li><a href="#">Plugins</a></li>
+          <li><a href="#">Suggest Ideas</a></li>
+          <li><a href="#">Support Forum</a></li>
+          <li><a href="#">Themes</a></li>
+          <li><a href="#">WordPress Blog</a></li>
+          <li><a href="#">WordPress Planet</a></li>
+        </ul>
+      </aside>
+    </div>
+
+
   </div>
 </template>
 
@@ -60,5 +80,38 @@
 
 
 <style scoped lang="scss">
+
+  aside {
+    background: aliceblue;;
+    color: #2d2d2d;
+    ul, ul.categories, ul.recent_comments, ul.blogroll {
+      font-size: 1.4em;
+      line-height: 2.2em;
+      color: #777;
+      list-style: square inside;
+    }
+     ul, ul.categories, ul.recent_comments, ul.blogroll {
+      font-size: 1.4em;
+      line-height: 2.2em;
+       list-style: square inside;
+     }
+  }
+
+  .container_16 {
+    .prefix_1 {
+      padding-left: 60px;
+    }
+    .grid_5 {
+      width: 280px;
+    }
+  }
+
+  h4, h5 {
+    margin: 35px 0 20px 10px;
+    color: #2d2d2d;
+    font-family: Georgia, "Times New Roman", Times, serif;
+    font-size: 2.4em;
+    text-align: justify;
+  }
 
 </style>
--- src/components/idx/Index.vue
+++ src/components/idx/Index.vue
@@ -57,7 +57,7 @@
                src="http://index.yulibaozi.com/ced9c5fe-4cf6-4212-a472-643b79e907d6yasuko.jpg" lazy="loaded">
         </a>
       </p>
-      <p class="note">
+      <p class="note-right">
         k8s中请求网络链路的分析与总结
 
         前要
@@ -79,46 +79,6 @@
         <span class="post-views"><i class="iconfont icon-yanjing"></i>阅读(74)</span>
         <span class="post-comments"><i class="iconfont icon-chat"></i>评论(5)</span>
       </p>
-    </div>
-
-    <div class="home-list">
-      <div class="home-list-li fadeIn s">
-        <header>
-          <h2>
-            <a>K8s中请求网络链路的分析与总结</a>
-          </h2>
-        </header>
-        <p class="text-muted views"><span>yulibaozi 发布于 2018/7/22</span></p>
-        <p class="focus">
-          <a href="/Article/63" class="">
-            <img title="K8s中请求网络链路的分析与总结"
-                 data-src="http://index.yulibaozi.com/ced9c5fe-4cf6-4212-a472-643b79e907d6yasuko.jpg"
-                 src="http://index.yulibaozi.com/ced9c5fe-4cf6-4212-a472-643b79e907d6yasuko.jpg" lazy="loaded">
-          </a>
-        </p>
-        <p class="note">
-          k8s中请求网络链路的分析与总结
-
-          前要
-          这篇文章，你能明白那些问题？
-          1. 从集群外部请求的时候，整个请求的走向是怎么样的？
-          2. 在service中port,targetPort和nodePort的区别是什么？
-          3. iptables是怎样工作的？
-          4. 谁来维护了数据的正确性，及时性和稳定性？
-
-          基础知识:
-
-          port指当使用ClusterIP访问这个服务所使用的port
-          target...
-        </p>
-        <p class="text-muted views">
-          <span>
-            <img src="http://user.yulibaozi.com/45fa6360-5df1-4967-87b9-5e532442556bQQ20180410-0.jpg">
-          </span>
-          <span class="post-views"><i class="iconfont icon-yanjing"></i>阅读(74)</span>
-          <span class="post-comments"><i class="iconfont icon-chat"></i>评论(5)</span>
-        </p>
-      </div>
     </div>
 
   </div>
@@ -207,7 +167,15 @@
     .note {
       color: #777;
       line-height: 1.5;
-      margin: 6px 10px 0;
+      margin: 6px 26% 0 10px;
+      padding-right: 15px;
+      word-break: break-all;
+      text-align: justify;
+    }
+    .note-right {
+      color: #777;
+      line-height: 1.5;
+      margin: 6px 25px 0 26.5%;
       padding-right: 15px;
       word-break: break-all;
       text-align: justify;
